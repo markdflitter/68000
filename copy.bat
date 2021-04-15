c52e0d0495bd784f2b3c932c127b8c7bbcc50470
markdflitter <markdflitter@googlemail.com>
2021-04-15 08:31:25 +0100

    more tidying up and attempt to move to c

--- copy.bat
+++ copy.bat
@@ -1,2 +1,2 @@
 mode com8: baud=38400 parity=n data=8
-copy main.S86 COM8:
+copy main.S68 COM8:
--- main.c
+++ main.c
@@ -1,4 +1,15 @@
+#define DISPLAY 0xE00000
+
+
+void f (char n)
+{
+	n = n / 2;
+	char* p = (char*) DISPLAY;
+	*p = n; 
+}
+
 void main (void)
 {
-	asm ("MOVE.B #9,0xE00001");
+	f ('\5');
+	//f (7);
 }
