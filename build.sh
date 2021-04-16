aef872ddf4872581968b7f774726b523bf341aa6
markdflitter <markdflitter@googlemail.com>
2021-04-16 13:04:31 +0100

    HelloWorld

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-m68k-elf-gcc main.c -o main.out -nostartfiles -nostdlib -Wl,--script=main.ld -Wl,--entry=main
+m68k-elf-gcc main.c -o main.out -nostartfiles -nostdlib -Wl,--script=main.ld -Wl,--entry=main -Wl,--print-map -march=68000
 m68k-elf-objcopy -O srec main.out main.S68
--- main.c
+++ main.c
@@ -1,14 +1,36 @@
+
 #define DISPLAY 0xE00001
-
-
-void f (char n)
+void setDisplay (char n)
 {
 	char* p = (char*) DISPLAY;
 	*p = n; 
 }
 
+#define TXA 0xC00007
+#define SRA 0xC00003
+#define TXREADY 0x4
+void putch (char c)
+{
+	volatile char* sra = (char*) SRA;
+	volatile char* txa = (char*) TXA;
+
+	while ((*sra & TXREADY) == 0x0)
+		;
+
+	*txa = c;
+}
+
+void print (char* s)
+{
+	while (*s != 0)
+	{
+		putch (*s++);
+	}
+}
+
+
 void main (void)
 {
-	f (6);
-	f (5);
+	print ("HelloWorld\r\n");
 }
+
