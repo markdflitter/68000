c05f3f610ef1e90cbdead1641acbeb51f4499bfa
Mark Flitter <markdflitter@googlemail.com>
2021-04-18 09:02:42 +0100

    still bootdstrapping

--- libc/install.sh
+++ libc/install.sh
@@ -2,5 +2,7 @@
 
 cp include/stdio.h /usr/local/crossgcc/include
 cp include/string.h /usr/local/crossgcc/include
+cp include/libgloss.h /usr/local/crossgcc/include
+
 cp bld/libc.a /usr/local/crossgcc/lib
 
