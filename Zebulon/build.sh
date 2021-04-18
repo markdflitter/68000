eeb447e11e73daadb8636bded5d562c93b885163
Mark Flitter <markdflitter@googlemail.com>
2021-04-18 10:05:52 +0100

    moved Zebulon to src/bld model

--- Zebulon/build.sh
+++ Zebulon/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-m68k-elf-gcc Zebulon.c -o Zebulon.out -I/usr/local/crossgcc/include -L/usr/local/crossgcc/lib -lc -lbsp -nostartfiles -nostdlib -Wl,--script=Zebulon.ld -Wl,--entry=main -Wl,--print-map -march=68000
-m68k-elf-objcopy -O srec Zebulon.out Zebulon.S68
+if [ ! -d "bld" ]
+then
+	mkdir bld
+fi
+ 
+if [ ! -d "bld/obj" ]
+then
+	mkdir bld/obj
+fi
+ 
+pushd bld/obj
+
+m68k-elf-gcc ../../src/Zebulon.c -o Zebulon.out -I/usr/local/crossgcc/include -L/usr/local/crossgcc/lib -lc -lbsp -nostartfiles -nostdlib -Wl,--script=../../src/Zebulon.ld -Wl,--entry=main -Wl,--print-map -march=68000
+
+m68k-elf-objcopy -O srec Zebulon.out ../Zebulon.S68
+
+popd
