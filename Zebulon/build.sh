f4576d9dc9afd76a170a2171df03620fefc0f1a4
Mark Flitter <markdflitter@googlemail.com>
2021-04-18 14:36:34 +0100

    reverted to nostdlib

--- Zebulon/build.sh
+++ Zebulon/build.sh
@@ -11,7 +11,7 @@ fi
  
 pushd bld/obj
 
-m68k-elf-gcc ../../src/Zebulon.c -o Zebulon.out -I/usr/local/crossgcc/include -L/usr/local/crossgcc/lib -lc -lbsp -Wl,--entry=main -nostdlib -Wl,--script=../../src/Zebulon.ld -march=68000
+m68k-elf-gcc ../../src/Zebulon.c -o Zebulon.out -march=68000 -nostdlib -I/usr/local/crossgcc/include -L/usr/local/crossgcc/lib -lc -lbsp -Wl,--script=../../src/Zebulon.ld -Wl,--entry=main -Wl,--print-map
 
 m68k-elf-objcopy -O srec Zebulon.out ../Zebulon.S68
 
