6f3a3d480365c80099570713d8c7ecb1a13e78e0
Mark Flitter <markdflitter@googlemail.com>
2021-04-18 09:02:29 +0100

    still bootdstrapping

--- libc/src/string.c
+++ libc/src/string.c
@@ -1,7 +1,31 @@
 #include "../include/string.h"
 
-int strlen (char* s)
-{
-	return 0;
+ int strlen (char* s)
+ {
+	int result = 0;
+
+ 	while (*s++ != '\0')
+ 		result++;
+
+	return result;
 }
+
+
+int strcmp (char* s1, char* s2)
+{
+	if (strlen (s1) != strlen (s2))
+		return 0;
+ 
+ 	while (*s1 != '\0')
+ 	{
+ 		if (*s1 != *s2)
+ 			return 0;
+ 		s1++;
+ 		s2++;
+	}
+
+	return 1;
+}
+
+
 
