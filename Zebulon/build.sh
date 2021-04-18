a87ea8715ba2a32b9dea16102c84143688622115
Mark Flitter <markdflitter@googlemail.com>
2021-04-18 07:53:06 +0100

    renamed

--- Zebulon/build.sh
+++ Zebulon/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-m68k-elf-gcc main.c -o main.out -nostartfiles -nostdlib -Wl,--script=main.ld -Wl,--entry=main -Wl,--print-map -march=68000
-m68k-elf-objcopy -O srec main.out main.S68
+m68k-elf-gcc Zebulon.c -o Zebulon.out -nostartfiles -nostdlib -Wl,--script=Zebulon.ld -Wl,--entry=main -Wl,--print-map -march=68000
+m68k-elf-objcopy -O srec Zebulon.out Zebulon.S68
--- Zebulon/copy.bat
+++ Zebulon/copy.bat
@@ -1,2 +1,2 @@
 mode com8: baud=38400 parity=n data=8
-copy main.S68 COM8:
+copy Zebulon.S68 COM8:
