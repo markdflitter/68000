7b1a89b345ab18709b45e0b3ab0e4e2328c0c556
Mark Flitter <markdflitter@googlemail.com>
2021-05-20 07:37:55 +0100

    add rw to duart

--- libbsp/include/rw.h
+++ libbsp/include/rw.h
@@ -1,15 +1,15 @@
-//#DEFINE LOG_ACCESS
-#UNDEF LOG_ACESS
+//#define LOG_ACCESS
+#undef LOG_ACESS
 
-#IFDEF LOG_ACCESS
+#if LOG_ACCESS
 #include <stdio.h>
-#ENDIF
+#endif
 
 inline void write (volatile unsigned char* address, unsigned char value)
 {
-#IFDEF LOG_ACCESS
+#ifdef LOG_ACCESS
 	printf ("write 0x%x to 0x%x\n\r", value, address);
-#ENDIF 
+#endif 
 
 	*address = value;
 }
@@ -18,9 +18,9 @@ inline unsigned char read (volatile unsigned char* address) const
 {
 	unsigned char value = *address;
 
-#IFDEF LOG_ACCESS
+#ifdef LOG_ACCESS
 	printf ("read 0x%x from 0x%x\n\r", value, address);
-#ENDIF
+#endif
 
 	return value;
 }
