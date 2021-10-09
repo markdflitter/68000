93cae4c2deaa4f1471d5cc7876f8f69a01a30cfd
Mark Flitter <markdflitter@googlemail.com>
2021-10-09 08:55:54 +0100

    implemented spaceManager

--- Zebulon/src/SpaceManager.cpp
+++ Zebulon/src/SpaceManager.cpp
@@ -16,6 +16,32 @@ int SpaceManager::initialise (int initialSize)
 	return initialSize;
 }
 
+std::list<Chunk::Ptr> SpaceManager::allocate (unsigned long size, bool contiguous)
+{
+	std::list<Chunk::Ptr> allocation;
+
+	if (size > 0)
+	{
+		if (m_free.size () > 0)
+		{
+			Chunk::Ptr first = *(m_free.begin ());
+			if (first->length >= size)
+			{
+				allocation.push_back (make_shared (new Chunk (first->start, size)));
+				first->start += size;
+				first->length -= size;
+			}
+		}
+	}
+
+	return allocation;
+}	
+
+void SpaceManager::deallocate (std::list<Chunk::Ptr>& chunks)
+{
+	m_free.splice (m_free.end (), chunks);
+}
+
 void SpaceManager::serialise (unsigned char*& p) const
 {
 	Serialise::serialise (m_free, p);
--- Zebulon/src/SpaceManager.h
+++ Zebulon/src/SpaceManager.h
@@ -12,6 +12,9 @@ class SpaceManager
 public:
 	int initialise (int initialSize);
 
+	std::list <Chunk::Ptr> allocate (unsigned long size, bool contiguous = false);
+	void deallocate (std::list<Chunk::Ptr>& chunks);
+
 	void serialise (unsigned char*& p) const;
 	void deserialise (const unsigned char*& p);
 private:	
