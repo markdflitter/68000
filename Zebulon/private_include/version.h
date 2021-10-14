b19c0780a10df06c7b6b77de9b9268409446da46
Mark Flitter <markdflitter@googlemail.com>
2021-10-14 19:01:34 +0100

    more alloc

--- Zebulon/private_include/version.h
+++ Zebulon/private_include/version.h
@@ -2,9 +2,9 @@
 #define MINOR "0"
 #define PATCH "0"
 
-#define BUILD_NUM "225"
+#define BUILD_NUM "231"
 #define BUILD_DATE "14 October 2021"
-#define BUILD_TIME "15:52:09"
+#define BUILD_TIME "19:00:07"
 
 #define VERSION MAJOR "." MINOR "." PATCH "." BUILD_NUM
 
--- libc/src/stdlib.c
+++ libc/src/stdlib.c
@@ -26,7 +26,7 @@ T max (const T& t1, const T& t2) { return t1 > t2 ? t1 : t2; }
 size_t roundUp (size_t input)
 {
 	for (size_t level = 1; level < MAX_HEAP_SIZE; level *= 2)
-		if (input < level) return level;
+		if (input <= level) return level;
 }
 
 }
@@ -43,7 +43,7 @@ static unsigned int free_count = zero ();
 void* malloc (size_t requestedSize)
 {
 	//add 4 bytes for length, min size 8, round up to next power of two
-	allocSize = roundUp (max (requestedSize + 4, 8));
+	size_t allocSize = roundUp (max (requestedSize + 4, 8UL));
 
 	char* alloc = top_of_heap;
 		
@@ -56,22 +56,22 @@ void* malloc (size_t requestedSize)
 		//printf (">> heapsize 0x%x - time to consider implementing delete\n\r",top_of_heap - (char*) &__end);
 	}
 
-	printf ("malloc %d -> %d 0x%x\n\r", requestedSize, allocSize, alloc);
-	
 	malloc_count++;
+	printf ("[%d] malloc %d -> %d 0x%x\n\r", malloc_count, requestedSize, allocSize, alloc);
+	
 	return (void*) alloc;
 }
 
 void free (void* ptr)
 {
 	free_count++;
-	//printf ("free 0x%x\n\r", ptr);
+	printf ("[%d] free 0x%x\n\r", free_count, ptr);
 }
 
 
 void heap_diag ()
 {
-	printf ("HEAP: base 0x%x, top 0x%x, limit 0x%x - used %d / %d (%d%%) [%d - %d = %d]\n\r", base_of_heap, top_of_heap, heap_limit, (top_of_heap - base_of_heap), MAX_HEAP_SIZE, 100 * (top_of_heap - base_of_heap) / MAX_HEAP_SIZE, malloc_count, free_count, malloc_count - free_count);
+	printf ("HEAP: base 0x%x, top 0x%x, limit 0x%x - used %d / %d (%d%%) [%d malloc'd - %d freed = %d allocated]\n\r", base_of_heap, top_of_heap, heap_limit, (top_of_heap - base_of_heap), MAX_HEAP_SIZE, 100 * (top_of_heap - base_of_heap) / MAX_HEAP_SIZE, malloc_count, free_count, malloc_count - free_count);
 }
 
 void abort (void)
